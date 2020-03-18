dca2c6b5f9552d25ad646db2d9a63481f7b67da5
BuildTools <unconfigured@null.spigotmc.org>
2020-03-18 17:15:03 +0100

    changements

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ client.on('message', msg => {
         console.log("'"+msg.content+"' was executed by "+msg.author.tag+" on '"+msg.guild.name+"'.'"+msg.channel.name+"' !");
         if (scmd === "listemojis") {
           if (msg.channel.id === "685181829577572423") {
-            const emojiList = msg.guild.emojis.map((e, x) => ({name:x,value:e+" = "+e.name}));
-            msg.channel.send({embed:{color:255,fields:emojiList}})
+            const emojiList = msg.guild.emojis.map((e, x) => ({"name":x,"value":e+" = "+e.name,"inline":true}));
+            msg.channel.send({embed:{color:16711680,fields:emojiList}})
           } else if (msg.guild.id === "681549703212564547") {
             console.log("ok")
             msg.channel.send("Petit malin ! Utilise cette commande sur "+msg.guild.channels.get('685181829577572423').toString()+" ! Cette commande est beaucoup trop encombrante. Ce serait le bordel. Comprenez-nous ! :cry:")
