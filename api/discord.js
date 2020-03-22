66a2ce2e6ef515520b4bd9d277329ebce6959283
BuildTools <unconfigured@null.spigotmc.org>
2020-03-22 12:37:24 +0100

    web

--- api/discord.js
+++ api/discord.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const btoa = require('btoa');
 const { catchAsync } = require('../utils');
 const router = express.Router();
+const CLIENT_ID = "684464572333293605";
+const CLIENT_SECRET = "R1RLwL30t4vMeFJtGGemNiMELzoef_r2";
+const redirect = encodeURIComponent('https://gregoland-chatbot.herokuapp.com/api/discord/callback');
 
 router.get('/login', (req, res) => {
 	res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify%20guilds&response_type=code&redirect_uri=${redirect}`);
