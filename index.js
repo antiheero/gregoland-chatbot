687f33629e45cf6ca661b7eccf8d9779572cbed7
Valentin Moguérou <valentin@moguerou.net>
2020-06-01 11:46:38 +0200

    Adding cow support

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const yaml = require('js-yaml');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
+
 const cool = require('cool-ascii-faces');
+const vaca = require("vaca");
+
 console.log("Logging in with this token : \""+BOT_TOKEN+"\" !")
 
 const prefix = ["!", "/"];
@@ -181,6 +184,8 @@ client.on('message', msg => {
 					msg.channel.send("Your avatar :",{files:[msg.author.avatarURL]})
 				} else if (scmd === "face") {
 					msg.channel.send(cool());
+				} else if (scmd === "cow") {
+					msg.channel.send(vaca());
 				} else if (scmd === "cr") {
 					const users = msg.mentions.users.array();
 					const user = users[0];
