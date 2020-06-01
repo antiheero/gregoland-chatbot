7e257539bc9d4a6a41ea694735973e5d75f41c56
goldenval56 <62617332+goldenval56@users.noreply.github.com>
2020-06-01 10:56:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on('guildMemberAdd', member => {
 		`Bienvenue sur le serveur <@${member.user.id}> !`
 	);
 	if (member.guild.id === "681549703212564547") {
-		member.addRole("682535382516105231")
+		member.addRole("681556751509880889")
 		member.send("Vive le Parti unique !\nTu as reçu ton rôle !\nLe Parti t'accorde 100 ₲ ! Pour les recevoir, fais : ```\n!eco\n```");
 	} else if (member.guild.id === "689476199172407440") {
 		member.addRole("689477316744970363");
